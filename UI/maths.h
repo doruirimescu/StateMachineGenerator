d2c160781affb57d3c87ba3ac602a7be703ceb73
Doru Irimescu <doru.irimescu@aalto.fi>
2020-04-12 10:19:33 +0300

    -Use only constexpr, remove defines that are not according to C++

--- UI/maths.h
+++ UI/maths.h
@@ -2,11 +2,13 @@
 #define MATHS_H
 #include <QtMath>
 #include <QPoint>
-#define PI_OVER_4       M_PI/4
-#define MINUS_PI_OVER_4 -M_PI/4
-#define THREE_PI_OVER_4 3*M_PI/4
-#define MINUS_THREE_PI_OVER_4 -3*M_PI/4
+
+constexpr double PI_OVER_4              =  M_PI/4;
+constexpr double MINUS_PI_OVER_4        = -M_PI/4;
+constexpr double THREE_PI_OVER_4        = 3*M_PI/4;
+constexpr double MINUS_THREE_PI_OVER_4  = -3*M_PI/4;
 constexpr QPoint invalidPoint = QPoint(-1000, -1000);
+
 class Maths
 {
 public:
