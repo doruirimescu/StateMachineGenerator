239a3d6892dc164c4de6e31dd1d376610674017b
Doru Irimescu <doru.irimescu@aalto.fi>
2020-04-12 10:40:02 +0300

    -Move setter-getters to header. Use const getters.

--- UI/state.h
+++ UI/state.h
@@ -15,16 +15,31 @@ public:
     State( QString label, QPoint pos, QPen pen, QColor color, int radius ) :  pen(pen), pos(pos), col(color), radius(radius), label(label){};
     ~State(){ qInfo()<<this->label<<"dead!"; }
 
-    QPoint getPos();
-    QPen getPen();
-    QColor getCol();
     void print();
-    void setLabel(QString label);
-    QString getLabel();
-    void setCode(QString code);
-    QString getCode();
-    int getRad();
-    void setRad(int r);
+
+    /* State position setter-getter */
+    void setPos(QPoint position) { pos = position; };
+    QPoint getPos() const { return pos; };
+
+    /* State pen setter-getter */
+    void setPen(QPen p){ pen = p; };
+    QPen getPen() const { return pen; };
+
+    /* State color setter-getter */
+    void setCol(QColor c){ col = c; };
+    QColor getCol() const { return col; };
+
+    /* Set label setter-getter */
+    void setLabel(QString l){ label = l; };
+    QString getLabel() const { return label; };
+
+    /* State code setter-getter */
+    void setCode(QString c){ code = c; };
+    QString getCode() const { return code; };
+
+    /* State radius setter-getter */
+    void setRad(int r){ radius = r; };
+    int getRad() const { return radius; };
 
 private:
     QString code; //code that runs when the state outputs
@@ -33,7 +48,6 @@ private:
     QColor col; //for drawing background
     int radius = 0; //radius of state
     QString label;
-
 };
 
 #endif // STATE_H
