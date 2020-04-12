ef618ef3af973dfce54993cd893074a3eb92c0ea
Doru Irimescu <doru.irimescu@aalto.fi>
2020-04-12 10:54:19 +0300

    -Move setter-getters to header. Use const getters.

--- UI/action.h
+++ UI/action.h
@@ -6,19 +6,27 @@ class Action
 {
 public:
     /* Constructor-destructor */
-    Action( State* s, State*e, QPoint sP, QPoint eP ) : start(s), end(e), startPos(sP), endPos(eP){};
+    Action( State* s, State* e, QPoint sP, QPoint eP ) : start(s), end(e), startPos(sP), endPos(eP){};
     ~Action(){qInfo()<<"Action from state: "<<this->start->getLabel()<<" to state: "<<this->end->getLabel()<<"DEAD";}
 
     void print();
-    State* getStart();
-    State* getEnd();
-    QPoint getStartPoint();
-    QPoint getEndPoint();
 
-    void setStart(State*);
-    void setEnd(State*);
-    void setStartPoint(QPoint*);
-    void setEndPoint(QPoint*);
+    /* Start state setter-getter */
+    void setStart(State* &s){ start = s; };
+    State* getStart() const { return start; };
+
+    /* End state setter-getter */
+    void setEnd(State* &e){ end = e; };
+    State* getEnd() const { return end; };
+
+    /* Start anchor position setter-getter */
+    void setStartPoint(QPoint* s){ startPos = *s; };
+    QPoint getStartPoint() const { return startPos; };
+
+    /* End anchor position setter-getter */
+    void setEndPoint(QPoint *e){ endPos = *e; };
+    QPoint getEndPoint() const { return endPos; };
+
 private:
     State* start;
     State* end;
