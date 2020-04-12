9d3518313734f5d3db0c05736798bbba62444d25
Doru Irimescu <doru.irimescu@aalto.fi>
2020-04-12 11:59:03 +0300

    -Remove 1.1 tolerance from intersectState

--- UI/manager.cpp
+++ UI/manager.cpp
@@ -68,12 +68,12 @@ bool Manager::intersectState(QPoint pos)
 {
     int r,x,y;
     for( const auto & i : states )
-    {
+    {/* Check all the states */
         r = i->getRad();
         x = i->getPos().x();
         y = i->getPos().y();
 
-        if( qFabs( pos.x() - x ) < 2*r*1.1 && qFabs( pos.y() - y ) < 2*r*1.1 )
+        if( qFabs( pos.x() - x ) < 2 * r && qFabs( pos.y() - y ) < 2 * r )
         {
             qInfo()<< "Intersect state ";
             return true;
