337719814baee3370817530c255fb4f0170ffc53
Doru Irimescu <doru.irimescu@aalto.fi>
2020-04-19 20:58:59 +0300

    Change replaceStart, replaceEnd to act only if there are enough splits.

--- UI/action.cpp
+++ UI/action.cpp
@@ -8,75 +8,79 @@ void Action::print()
 
 void Action::replaceStart()
 {/* Action start state position has been replaced */
-    QPoint newPos = start->getPos();
-
-    if( startAnchor.compare("R") == 0)
+    if( splits.size() > 2 )
     {
-        startPos = newPos + QPoint( start->getRad(), 0 );
-        splits.replace(0, startPos);
-        QPoint s2 = splits.at(1);
-        s2.setY( newPos.y());
-        splits.replace(1, s2 );
-    }
-    else if( startAnchor.compare("L") == 0 )
-    {
-        startPos = newPos - QPoint( start->getRad(), 0 );
-        splits.replace(0, startPos);
-        QPoint s2 = splits.at(1);
-        s2.setY( newPos.y());
-        splits.replace(1, s2 );
-    }
-    else if( startAnchor.compare("U") == 0 )
-    {
-        startPos = newPos - QPoint( 0, start->getRad() );
-        splits.replace(0, startPos);
-        QPoint s2 = splits.at(1);
-        s2.setY( newPos.y() - start->getRad() );
-        splits.replace(1, s2 );  
-    }
-    else if( startAnchor.compare("D") == 0 )
-    {
-        startPos = newPos + QPoint( 0, start->getRad() );
-        splits.replace(0, startPos);
-        QPoint s2 = splits.at(1);
-        s2.setY( newPos.y() + start->getRad() );
-        splits.replace(1, s2 );
+        QPoint newPos = start->getPos();
+        if( startAnchor.compare("R") == 0)
+        {
+            startPos = newPos + QPoint( start->getRad(), 0 );
+            splits.replace(0, startPos);
+            QPoint s2 = splits.at(1);
+            s2.setY( newPos.y());
+            splits.replace(1, s2 );
+        }
+        else if( startAnchor.compare("L") == 0 )
+        {
+            startPos = newPos - QPoint( start->getRad(), 0 );
+            splits.replace(0, startPos);
+            QPoint s2 = splits.at(1);
+            s2.setY( newPos.y());
+            splits.replace(1, s2 );
+        }
+        else if( startAnchor.compare("U") == 0 )
+        {
+            startPos = newPos - QPoint( 0, start->getRad() );
+            splits.replace(0, startPos);
+            QPoint s2 = splits.at(1);
+            s2.setY( newPos.y() - start->getRad() );
+            splits.replace(1, s2 );
+        }
+        else if( startAnchor.compare("D") == 0 )
+        {
+            startPos = newPos + QPoint( 0, start->getRad() );
+            splits.replace(0, startPos);
+            QPoint s2 = splits.at(1);
+            s2.setY( newPos.y() + start->getRad() );
+            splits.replace(1, s2 );
+        }
     }
 }
 void Action::replaceEnd()
 {
-    QPoint newPos = end->getPos();
-    if( endAnchor.compare("R") == 0)
+    if( splits.size() > 2 )
     {
-        endPos = newPos + QPoint( end->getRad(), 0 );
-        splits.replace(splits.size() - 1, endPos);
-        QPoint s2 = splits.at( splits.size() - 2 );
-        s2.setY( newPos.y() );
-        splits.replace(splits.size() - 2, s2 );
+        QPoint newPos = end->getPos();
+        if( endAnchor.compare("R") == 0)
+        {
+            endPos = newPos + QPoint( end->getRad(), 0 );
+            splits.replace(splits.size() - 1, endPos);
+            QPoint s2 = splits.at( splits.size() - 2 );
+            s2.setY( newPos.y() );
+            splits.replace(splits.size() - 2, s2 );
+        }
+        else if( endAnchor.compare("L") == 0 )
+        {
+            endPos = newPos - QPoint( end->getRad(), 0 );
+            splits.replace(splits.size() - 1, endPos);
+            QPoint s2 = splits.at( splits.size() - 2 );
+            s2.setY( newPos.y() );
+            splits.replace(splits.size() - 2, s2 );
+        }
+        else if( endAnchor.compare("U") == 0 )
+        {
+            endPos = newPos - QPoint( 0, end->getRad() );
+            splits.replace(splits.size() - 1, endPos);
+            QPoint s2 = splits.at( splits.size() - 2 );
+            s2.setY( newPos.y() - end->getRad() );
+            splits.replace(splits.size() - 2, s2 );
+        }
+        else if( startAnchor.compare("D") == 0 )
+        {
+            endPos = newPos + QPoint( 0, end->getRad() );
+            splits.replace(splits.size() - 1, endPos);
+            QPoint s2 = splits.at( splits.size() - 2 );
+            s2.setY( newPos.y() + end->getRad() );
+            splits.replace(splits.size() - 2, s2 );
+        }
     }
-    else if( endAnchor.compare("L") == 0 )
-    {
-        endPos = newPos - QPoint( end->getRad(), 0 );
-        splits.replace(splits.size() - 1, endPos);
-        QPoint s2 = splits.at( splits.size() - 2 );
-        s2.setY( newPos.y() );
-        splits.replace(splits.size() - 2, s2 );
-    }
-    else if( endAnchor.compare("U") == 0 )
-    {
-        endPos = newPos - QPoint( 0, end->getRad() );
-        splits.replace(splits.size() - 1, endPos);
-        QPoint s2 = splits.at( splits.size() - 2 );
-        s2.setY( newPos.y() - end->getRad() );
-        splits.replace(splits.size() - 2, s2 );
-    }
-    else if( startAnchor.compare("D") == 0 )
-    {
-        endPos = newPos + QPoint( 0, end->getRad() );
-        splits.replace(splits.size() - 1, endPos);
-        QPoint s2 = splits.at( splits.size() - 2 );
-        s2.setY( newPos.y() + end->getRad() );
-        splits.replace(splits.size() - 2, s2 );
-    }
-
 }
